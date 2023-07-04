a8807b8c96d464391ffa1f202437ae3008ab4efd
rodrigopineda2902 <114821265+rodrigopineda2902@users.noreply.github.com>
2023-07-04 04:44:13 -0500

    Create SaldoUsuarioService.java

---
+++ CODIGO JAVA/appventasAngular/backrest/src/main/java/uni/edu/pe/appventas/service/SaldoUsuarioService.java
@@ -0,0 +1,9 @@
+package uni.edu.pe.appventas.service;
+
+import uni.edu.pe.appventas.dto.SaldoUsuario;
+
+import java.util.List;
+
+public interface SaldoUsuarioService {
+    List<SaldoUsuario> mostrarSaldoUsuario();
+}
