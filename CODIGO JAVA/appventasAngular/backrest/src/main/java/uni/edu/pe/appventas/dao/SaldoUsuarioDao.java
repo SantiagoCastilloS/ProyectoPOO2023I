65ca08aa8ceac5f9461a38f9b5a429ff195126b6
rodrigopineda2902 <114821265+rodrigopineda2902@users.noreply.github.com>
2023-07-04 04:28:04 -0500

    Create SaldoUsuarioDao.java

---
+++ CODIGO JAVA/appventasAngular/backrest/src/main/java/uni/edu/pe/appventas/dao/SaldoUsuarioDao.java
@@ -0,0 +1,9 @@
+package uni.edu.pe.appventas.dao;
+
+import uni.edu.pe.appventas.dto.SaldoUsuario;
+
+import java.util.List;
+
+public interface SaldoUsuarioDao {
+    List<SaldoUsuarioDao> mostrarsaldoUsuarioDao();
+}
