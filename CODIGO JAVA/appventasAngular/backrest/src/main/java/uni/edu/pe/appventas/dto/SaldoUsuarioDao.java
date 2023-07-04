23469457c8f86d22803bd05eefa4c43170d1a421
rodrigopineda2902 <114821265+rodrigopineda2902@users.noreply.github.com>
2023-07-04 04:27:28 -0500

    Update SaldoUsuarioDao.java

--- CODIGO JAVA/appventasAngular/backrest/src/main/java/uni/edu/pe/appventas/dto/SaldoUsuarioDao.java
+++ CODIGO JAVA/appventasAngular/backrest/src/main/java/uni/edu/pe/appventas/dto/SaldoUsuarioDao.java
@@ -1,9 +1,1 @@
-package uni.edu.pe.appventas.dao;
 
-import uni.edu.pe.appventas.dto.SaldoUsuario;
-
-import java.util.List;
-
-public interface SaldoUsuarioDao {
-    List<SaldoUsuarioDao> mostrarsaldoUsuarioDao();
-}
