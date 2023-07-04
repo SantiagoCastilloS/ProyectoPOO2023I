0e1c479689ff9db792ee0f9ecb38b493a47b66bf
rodrigopineda2902 <114821265+rodrigopineda2902@users.noreply.github.com>
2023-07-04 04:35:08 -0500

    Update DetalleUsuarioDaoImp.java

--- CODIGO JAVA/appventasAngular/backrest/src/main/java/uni/edu/pe/appventas/dao/DetalleUsuarioDaoImp.java
+++ CODIGO JAVA/appventasAngular/backrest/src/main/java/uni/edu/pe/appventas/dao/DetalleUsuarioDaoImp.java
@@ -33,7 +33,7 @@ public class DetalleUsuarioDaoImp implements DetalleUsuarioDao{
     }
     @Override
     public List<DetalleUsuario> mostrardetalleUsuario() {
-        List<DetalleUsuario> detalleUsuario = new ArrayList<>();
+        List<DetalleUsuario> detalleUsuarios = new ArrayList<>();
         try{
             obtConeccion();
             String sql = "SELECT u.nombre_usuario, pr.nombre_prod, pr.precio_prod, dc.cantidad, p.fecha_creacion_ped 'fecha_pedido', p.hora_creacion_ped 'hora_pedido', te.nombre_envio, sp.estado_seguimiento 'estado_seguimiento' FROM usuario u INNER JOIN carrito c ON (u.id_usuario = c.id_usuario) INNER JOIN detalle_carrito dc ON (c.id_carrito = dc.id_carrito) INNER JOIN producto pr ON (dc.id_prod = pr.id_prod) INNER JOIN pedido p ON (u.id_usuario = p.id_usuario ) INNER JOIN pago pa ON (p.id_pedido = pa.id_pedido) INNER JOIN envio en ON (p.id_envio = en.id_envio) INNER JOIN tipo_envio te ON (en.id_tipo_envio = te.id_tipo_envio) INNER JOIN seguimiento_pedido sp ON (sp.id_pedido = p.id_pedido) WHERE (u.nombre_usuario = ?)";
@@ -41,7 +41,7 @@ public class DetalleUsuarioDaoImp implements DetalleUsuarioDao{
             ResultSet rs = st.executeQuery();
             while (rs.next()){
               DetalleUsuario detalleUsuario = new DetalleUsuario(rs.getString("nombre_usuario"),rs.getString("nombre_prod"),rs.getFloat("precio_prod"),rs.getInt("cantidad"),rs.getChar("fecha_creacion_ped"),rs.getChar("hora_creacion_ped"),rs.getString("nombre_envio"),rs.getChar("estado_seguimiento"));
-                detalleUsuario.add(detalleUsuario);
+                detalleUsuarios.add(detalleUsuario);
             }
             st.close();
             rs.close();
@@ -49,6 +49,6 @@ public class DetalleUsuarioDaoImp implements DetalleUsuarioDao{
         } catch (SQLException e){
             e.printStackTrace();
         }
-        return detalleUsuario;
+        return detalleUsuarios;
     }
 }
